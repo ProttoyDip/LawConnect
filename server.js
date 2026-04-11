fa2ebef2e956adfc0b015779d221f8a697736282
Prottoy Saha Dip <prottoysahadip2@gmail.com.com>
2026-04-12 03:57:40 +0600

    Make CORS middleware compatible with credentialed browser requests

--- server.js
+++ server.js
@@ -9,9 +9,19 @@ const app = express();
 const port = Number(process.env.PORT || 3000);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  const origin = req.headers.origin;
+  if (origin) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Vary', 'Origin');
+    res.header('Access-Control-Allow-Credentials', 'true');
+  } else {
+    res.header('Access-Control-Allow-Origin', '*');
+  }
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept');
+  res.header(
+    'Access-Control-Allow-Headers',
+    req.headers['access-control-request-headers'] || 'Content-Type, Authorization, Accept'
+  );
 
   if (req.method === 'OPTIONS') {
     return res.sendStatus(204);
